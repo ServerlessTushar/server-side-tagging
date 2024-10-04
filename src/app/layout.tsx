b24903b2a67f5efcb6279cf74e32a97130d5154d
firstPenguin1988 <tusharparmarofficial@gmail.com>
2024-10-04 12:10:55 +0530

    undo new tag id change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             '/js/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KS33ZRQV');`,
+            })(window,document,'script','dataLayer','GTM-T9BNSS83');`,
           }}></Script>
           
       {/* <GoogleTagManager gtmId={'GTM-T9BNSS83'} serverSideContainerUrl={'https://localhost:3003/api/gtm'}/> */}
